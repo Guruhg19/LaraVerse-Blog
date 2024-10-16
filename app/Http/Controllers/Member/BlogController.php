5ce42d9912702b2159980c808900a52dedfdd12b
GurruhhVi <guruhvitra1@gmail.com>
2024-10-17 06:45:49 +0700

    Create index.blade.php in member folder and modified BlogController

--- app/Http/Controllers/Member/BlogController.php
+++ app/Http/Controllers/Member/BlogController.php
@@ -2,9 +2,10 @@
 
 namespace App\Http\Controllers\Member;
 
-use App\Http\Controllers\Controller;
 use App\Models\Post;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
 
 class BlogController extends Controller
 {
@@ -13,8 +14,9 @@ class BlogController extends Controller
      */
     public function index()
     {
-        $data = Post::all();
-        print_r($data);
+        $user = Auth::user();
+        $data = Post::where('user_id', $user->id)->orderBy('id','desc')->get();
+        return view('member.blogs.index',compact('data'));
     }
 
     /**
